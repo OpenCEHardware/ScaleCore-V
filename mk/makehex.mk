6255c7bb4f2ea9ec7b125f9d5dbb3a5fa0092885
Alejandro Soto <alejandro@34project.org>
2024-10-15 02:23:45 -0600

    mk: update bin2hex script

--- mk/makehex.mk
+++ mk/makehex.mk
@@ -3,8 +3,8 @@ makehex_obj = $(call require_core_objs,$(1),makehex_obj)
 
 define hooks/makehex
   define obj_rules
-    $$(call makehex_obj,$(1)): $$(call makehex_src,$(1)) scripts/makehex.py $$(obj_deps)
-		$$(call run,MAKEHEX,$$@) $$(PYTHON3) scripts/makehex.py <$$< >$$@
+    $$(call makehex_obj,$(1)): $$(call makehex_src,$(1)) mk/scripts/bin2hex.py $$(obj_deps)
+		$$(call run,BIN2HEX,$$@) $$(PYTHON3) mk/scripts/bin2hex.py -b $$< -o $$@
   endef
 
   $$(eval $$(call add_obj_rules,$(1)))
